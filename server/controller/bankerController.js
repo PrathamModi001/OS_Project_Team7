35d7804bef7df11d81bda86962d306993b2a21fa
Pras2214 <parikhprasann@gmail.com>
2023-02-22 11:31:01 +0530

    added toastify

--- server/controller/bankerController.js
+++ server/controller/bankerController.js
@@ -1,3 +1,5 @@
+const Banker = require("../models/banker");
+
 /** GET Dead Lock page */
 exports.getBanker = (req, res, next) => {
   res.render("banker", {
@@ -11,6 +13,7 @@ exports.postBanker = (req, res, next) => {
   let index = 0;
   let availableIndex = 0;
   let flag = 0;
+  let checkDiff = 1;
   let allNotDone = true;
   let isDeadLock = false;
   let available = {
@@ -19,179 +22,194 @@ exports.postBanker = (req, res, next) => {
     C: 0,
   };
   let processNumArr = req.body.pNum;
-  let processSequence = [];
-  const A = req.body.a;
-  const B = req.body.b;
-  const C = req.body.c;
-  let totalAllocatedA = 0;
-  let totalAllocatedB = 0;
-  let totalAllocatedC = 0;
-
-  class process {
-    done = false;
-    processNum = 0;
-    allocated = {
-      A: 0,
-      B: 0,
-      C: 0,
-    };
-    maxNeed = {
-      A: 0,
-      B: 0,
-      C: 0,
-    };
-    remainingNeed = {
-      A: 0,
-      B: 0,
-      C: 0,
-    };
-
-    constructor(
-      allocatedA,
-      allocatedB,
-      allocatedC,
-      maxNeedA,
-      maxNeedB,
-      maxNeedC,
-      processNum
-    ) {
-      this.allocated.A = allocatedA;
-      this.allocated.B = allocatedB;
-      this.allocated.C = allocatedC;
-
-      this.maxNeed.A = maxNeedA;
-      this.maxNeed.B = maxNeedB;
-      this.maxNeed.C = maxNeedC;
-
-      this.processNum = processNum;
-    }
-  }
-
-  let AllocatedA = [];
-  let AllocatedB = [];
-  let AllocatedC = [];
-  let MaxNeedA = [];
-  let MaxNeedB = [];
-  let MaxNeedC = [];
-  let RemainingNeedA = [];
-  let RemainingNeedB = [];
-  let RemainingNeedC = [];
-  let AvailableA = [];
-  let AvailableB = [];
-  let AvailableC = [];
-
-  AllocatedA = req.body.allocA;
-  AllocatedB = req.body.allocB;
-  AllocatedC = req.body.allocC;
-
-  MaxNeedA = req.body.maxA;
-  MaxNeedB = req.body.maxB;
-  MaxNeedC = req.body.maxC;
-
-  function calculateRemainingNeed(p) {
-    p.remainingNeed.A = p.maxNeed.A - p.allocated.A;
-    p.remainingNeed.B = p.maxNeed.B - p.allocated.B;
-    p.remainingNeed.C = p.maxNeed.C - p.allocated.C;
-    return;
-  }
 
   for (let i = 0; i < processNumArr.length; i++) {
-    let p = new process(
-      AllocatedA[i],
-      AllocatedB[i],
-      AllocatedC[i],
-      MaxNeedA[i],
-      MaxNeedB[i],
-      MaxNeedC[i],
-      processNumArr[i]
-    );
-    calculateRemainingNeed(p);
-    RemainingNeedA[i] = p.remainingNeed.A;
-    RemainingNeedB[i] = p.remainingNeed.B;
-    RemainingNeedC[i] = p.remainingNeed.C;
-    arr.push(p);
-  }
-
-  function calculateAvailableFirstTime() {
-    for (let i = 0; i < processNumArr.length; i++) {
-      totalAllocatedA += parseInt(AllocatedA[i]);
-      totalAllocatedB += parseInt(AllocatedB[i]);
-      totalAllocatedC += parseInt(AllocatedC[i]);
-    }
-    available.A = A - totalAllocatedA;
-    available.B = B - totalAllocatedB;
-    available.C = C - totalAllocatedC;
-
-    AvailableA[availableIndex] = available.A;
-    AvailableB[availableIndex] = available.B;
-    AvailableC[availableIndex] = available.C;
-    availableIndex++;
-
-    return;
-  }
-
-  function checkNeed(available, remaining_need_process) {
-    if (
-      parseInt(available.A) >= parseInt(remaining_need_process.A) &&
-      parseInt(available.B) >= parseInt(remaining_need_process.B) &&
-      parseInt(available.C) >= parseInt(remaining_need_process.C)
-    ) {
-      return true;
-    }
-    return false;
-  }
-
-  function allDone() {
-    for (let k = 0; k < arr.length; k++) {
-      if (arr[k].done === false) {
-        return;
-      }
-    }
-    allNotDone = false;
-    return;
-  }
-
-  function calculateAvailable(i) {
-    available.A += parseInt(AllocatedA[i]);
-    available.B += parseInt(AllocatedB[i]);
-    available.C += parseInt(AllocatedC[i]);
-    AvailableA[availableIndex] = available.A;
-    AvailableB[availableIndex] = available.B;
-    AvailableC[availableIndex] = available.C;
-    availableIndex++;
-    return;
-  }
-  calculateAvailableFirstTime();
-
-  while (allNotDone) {
-    if (arr.length === 0) {
-      break;
-    }
-    if (checkNeed(available, arr[index].remainingNeed) && !arr[index].done) {
-      flag = 0;
-      calculateAvailable(index);
-      processSequence.push(arr[index].processNum);
-      arr[index].done = true;
-      index = (index + 1) % arr.length;
-    } else {
-      index = (index + 1) % arr.length;
-      if (flag == arr.length) {
-        allNotDone = false;
+    for (let j = i + 1; j < processNumArr.length; j++) {
+      if (processNumArr[i] === processNumArr[j]) {
+        checkDiff = 0;
         break;
       }
-      flag += 1;
+    }
+  }
+  if (checkDiff === 1) {
+    //ACTUAL ALGO
+
+    let processSequence = [];
+    const A = req.body.a;
+    const B = req.body.b;
+    const C = req.body.c;
+    let totalAllocatedA = 0;
+    let totalAllocatedB = 0;
+    let totalAllocatedC = 0;
+    let AllocatedA = [];
+    let AllocatedB = [];
+    let AllocatedC = [];
+    let MaxNeedA = [];
+    let MaxNeedB = [];
+    let MaxNeedC = [];
+    let RemainingNeedA = [];
+    let RemainingNeedB = [];
+    let RemainingNeedC = [];
+    let AvailableA = [];
+    let AvailableB = [];
+    let AvailableC = [];
+
+    AllocatedA = req.body.allocA;
+    AllocatedB = req.body.allocB;
+    AllocatedC = req.body.allocC;
+
+    MaxNeedA = req.body.maxA;
+    MaxNeedB = req.body.maxB;
+    MaxNeedC = req.body.maxC;
+
+    class process {
+      done = false;
+      processNum = 0;
+      allocated = {
+        A: 0,
+        B: 0,
+        C: 0,
+      };
+      maxNeed = {
+        A: 0,
+        B: 0,
+        C: 0,
+      };
+      remainingNeed = {
+        A: 0,
+        B: 0,
+        C: 0,
+      };
+
+      constructor(
+        allocatedA,
+        allocatedB,
+        allocatedC,
+        maxNeedA,
+        maxNeedB,
+        maxNeedC,
+        processNum
+      ) {
+        this.allocated.A = allocatedA;
+        this.allocated.B = allocatedB;
+        this.allocated.C = allocatedC;
+
+        this.maxNeed.A = maxNeedA;
+        this.maxNeed.B = maxNeedB;
+        this.maxNeed.C = maxNeedC;
+
+        this.processNum = processNum;
+      }
     }
 
-    allDone();
-  }
-  if (flag === arr.length) {
-    console.log("DEADLOCK");
-    // console.log(processSequence);
-    isDeadLock = true;
+    function calculateRemainingNeed(p) {
+      p.remainingNeed.A = p.maxNeed.A - p.allocated.A;
+      p.remainingNeed.B = p.maxNeed.B - p.allocated.B;
+      p.remainingNeed.C = p.maxNeed.C - p.allocated.C;
+      return;
+    }
+
+    for (let i = 0; i < processNumArr.length; i++) {
+      let p = new process(
+        AllocatedA[i],
+        AllocatedB[i],
+        AllocatedC[i],
+        MaxNeedA[i],
+        MaxNeedB[i],
+        MaxNeedC[i],
+        processNumArr[i]
+      );
+      calculateRemainingNeed(p);
+      RemainingNeedA[i] = p.remainingNeed.A;
+      RemainingNeedB[i] = p.remainingNeed.B;
+      RemainingNeedC[i] = p.remainingNeed.C;
+      arr.push(p);
+    }
+
+    function calculateAvailableFirstTime() {
+      for (let i = 0; i < processNumArr.length; i++) {
+        totalAllocatedA += parseInt(AllocatedA[i]);
+        totalAllocatedB += parseInt(AllocatedB[i]);
+        totalAllocatedC += parseInt(AllocatedC[i]);
+      }
+      available.A = A - totalAllocatedA;
+      available.B = B - totalAllocatedB;
+      available.C = C - totalAllocatedC;
+
+      AvailableA[availableIndex] = available.A;
+      AvailableB[availableIndex] = available.B;
+      AvailableC[availableIndex] = available.C;
+      availableIndex++;
+
+      return;
+    }
+
+    function checkNeed(available, remaining_need_process) {
+      if (
+        parseInt(available.A) >= parseInt(remaining_need_process.A) &&
+        parseInt(available.B) >= parseInt(remaining_need_process.B) &&
+        parseInt(available.C) >= parseInt(remaining_need_process.C)
+      ) {
+        return true;
+      }
+      return false;
+    }
+
+    function allDone() {
+      for (let k = 0; k < arr.length; k++) {
+        if (arr[k].done === false) {
+          return;
+        }
+      }
+      allNotDone = false;
+      return;
+    }
+
+    function calculateAvailable(i) {
+      available.A += parseInt(AllocatedA[i]);
+      available.B += parseInt(AllocatedB[i]);
+      available.C += parseInt(AllocatedC[i]);
+      AvailableA[availableIndex] = available.A;
+      AvailableB[availableIndex] = available.B;
+      AvailableC[availableIndex] = available.C;
+      availableIndex++;
+      return;
+    }
+    calculateAvailableFirstTime();
+
+    while (allNotDone) {
+      if (arr.length === 0) {
+        break;
+      }
+      if (checkNeed(available, arr[index].remainingNeed) && !arr[index].done) {
+        flag = 0;
+        calculateAvailable(index);
+        processSequence.push(arr[index].processNum);
+        arr[index].done = true;
+        index = (index + 1) % arr.length;
+      } else {
+        index = (index + 1) % arr.length;
+        if (flag == arr.length) {
+          allNotDone = false;
+          break;
+        }
+        flag += 1;
+      }
+
+      allDone();
+    }
+    if (flag === arr.length) {
+      console.log("DEADLOCK");
+      // console.log(processSequence);
+      isDeadLock = true;
+    } else {
+      console.log(processSequence);
+    }
+    console.log(flag);
+    console.log(AvailableA);
+    console.log("BARABAR CHE!!!!");
   } else {
-    console.log(processSequence);
+    // alert("Enter Different process numbers.")
+    res.redirect("/");
   }
-  console.log(flag);
-  console.log(AvailableA);
-  res.redirect("/");
 };
--- server/controller/rrController.js
+++ server/controller/rrController.js
@@ -1,197 +1,170 @@
-const Result = require("../models/table")
+const Result = require("../models/table");
 
 /** GET Round Robin page */
 exports.getRoundRobin = (req, res, next) => {
-    res.render("round-robin", {
-        title: "Round-Robin"
-    })
-}
+  res.render("round-robin", {
+    title: "Round-Robin",
+  });
+};
 
 exports.postRoundRobin = async (req, res, next) => {
-    const pId = req.body.pId;
-    const at = req.body.at;
-    const bt = req.body.bt;
-    let quantum = parseInt(req.body.quantum);
-    let flag = 1;
-    let arr = [];
-    let time = 0;
-    
-    for (let i = 0; i < pId.length; i++) {
-        for (let j = i + 1; j < pId.length; j++) {
-            if (pId[i] === pId[j]) {
-                // alert("You must enter at least two processes to calculate.");
-                // event.preventDefault(); // prevent form submission
-                // console.log("SAME SAME SAME")
-                flag = 0;
-                break;
-            }
+  const pId = req.body.pId;
+  const at = req.body.at;
+  const bt = req.body.bt;
+  let quantum = parseInt(req.body.quantum);
+  let flag = 1;
+  let arr = [];
+  let time = 0;
+
+  for (let i = 0; i < pId.length; i++) {
+    for (let j = i + 1; j < pId.length; j++) {
+      if (pId[i] === pId[j]) {
+        flag = 0;
+        break;
+      }
+    }
+  }
+
+  if (flag === 1) {
+    // ACTUAL ALGO:
+
+    let processes = [];
+    let alldone = false;
+    class process {
+      index = 0;
+      id = 0;
+      at = 0;
+      bt = 0;
+      ct = 0;
+      tat = 0;
+      wt = 0;
+      done = false;
+      added = false;
+      constructor(at, bt, id) {
+        this.at = at;
+        this.bt = bt;
+        this.id = id;
+        this.done = false;
+      }
+      addTo(array) {
+        for (let j = 0; j < array.length; j++) {
+          if (this.id > array[j].id) {
+            continue;
+          } else {
+            array.splice(j, 0, this);
+            this.added = true;
+            return j;
+          }
         }
+        if (this.added === false) {
+          array.push(this);
+          this.added = true;
+          return array.length - 1;
+        }
+      }
+    }
+    const temp = [];
+
+    for (let i = 0; i < at.length; i++) {
+
+      processes[i] = new process(at[i], bt[i], pId[i]);
     }
 
-    if (flag === 1) {
-
-
-        // ACTUAL ALGO: 
-
-        let processes = [];
-        // let time = 0;
-        let alldone = false;
-        class process {
-            index = 0;
-            id = 0;
-            at = 0;
-            bt = 0;
-            ct = 0;
-            tat = 0;
-            wt = 0;
-            done = false;
-            added = false;
-            constructor(at, bt, id) {
-                this.at = at;
-                this.bt = bt;
-                this.id = id;
-                this.done = false;
-            }
-            addTo(array) {
-                for (let j = 0; j < array.length; j++) {
-                    if (this.id > array[j].id) {
-                        continue;
-                    } else {
-                        array.splice(j, 0, this);
-                        this.added = true;
-                        return j;
-                    }
-                }
-                if (this.added === false) {
-                    array.push(this);
-                    this.added = true;
-                    return array.length - 1;
-                }
-            }
-        }
-        const temp = [];
-
-        for (let i = 0; i < at.length; i++) {
-            // if(at.length==2){
-            //     processes[i] = new process(at[i], bt[i], pId[i]);
-            //     return;
-            // }
-            processes[i] = new process(at[i], bt[i], pId[i]);
-            // console.log(at);
-            // console.log(at[i]);
-        }
-
-        // let p2 = new process(0, 4, 5, 'p2');
-        // let p3 = new process(8, 3, 0, 'p3');
-        // let p4 = new process(60, 2, 2, 'p4');
-        for (let i = 0; i < processes.length; i++) {
-            temp.push(processes[[i]]);
-        }
-        // temp.push(p2);
-        // temp.push(p3);
-        // temp.push(p4);
-
-        // let arr = []
-        let BT = [];
-
-        function arrange() {
-            let i = 0;
-            let k = 0;
-            let idx = 0;
-            while (k < temp.length) {
-                if (temp[i].at <= time && temp[i].added === false) {
-                    if (arr.length == 0) {
-                        arr.push(temp[i]);
-                        BT.push(temp[i].bt);
-                        temp[i].added = true;
-                    }
-                    else {
-                        idx = temp[i].addTo(arr);
-                        BT.splice(idx, 0, temp[i].bt);
-                    }
-                }
-                i = (i + 1) % temp.length;
-                k++;
-            }
-            return;
-        }
-
-        function checkDone(checkArr) {
-            for (let x = 0; x < checkArr.length; x++) {
-                if (checkArr[x].done === false) {
-                    alldone = false;
-                    return;
-                }
-            }
-            alldone = true;
-        }
-
-        function isBTzero(Btarr) {
-            for (let y = 0; y < Btarr.length; y++) {
-                if (Btarr[y].bt !== 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        function Roundrobin() {
-            let i = 0
-            arrange();
-            while (!alldone) {
-                arrange();
-                checkDone(temp);
-                if (alldone === true) {
-                    break;
-                }
-                if (isBTzero(arr)) {
-                    // console.log("1");
-                    time++;
-                    i = 0;
-                    continue;
-                }
-                else {
-                    // console.log("2");
-                    if (arr[i].done == false) {
-                        // console.log("3");
-                        if (arr[i].bt <= quantum) {
-                            // console.log("4");
-                            time += arr[i].bt;
-                            arr[i].bt = 0;
-                            arr[i].ct = time;
-                            arr[i].tat = arr[i].ct - arr[i].at;
-                            arr[i].wt = arr[i].tat - BT[i];
-                            arr[i].done = true;
-                        }
-                        else {
-                            // console.log("5");
-                            arr[i].bt -= quantum;
-                            time += quantum;
-                        }
-                    }
-                }
-                i = (i + 1) % arr.length;
-            }
-            for (let i = 0; i < arr.length; i++) {
-                arr[i].bt = BT[i];
-            }
-        }
-
-        Roundrobin();
-
-        const newTable = new Result({
-            result: arr
-        })
-
-        await newTable.save()
-        // res.redirect("/round-robin/solution")
-        res.render("rrSolution", {
-            title: "RR Simulator",
-            arr: arr,
-            time: time,
-            flag : flag,
-        })
-    } else {
-        res.redirect("/")
+    for (let i = 0; i < processes.length; i++) {
+      temp.push(processes[[i]]);
     }
-}
+
+    let BT = [];
+
+    function arrange() {
+      let i = 0;
+      let k = 0;
+      let idx = 0;
+      while (k < temp.length) {
+        if (temp[i].at <= time && temp[i].added === false) {
+          if (arr.length == 0) {
+            arr.push(temp[i]);
+            BT.push(temp[i].bt);
+            temp[i].added = true;
+          } else {
+            idx = temp[i].addTo(arr);
+            BT.splice(idx, 0, temp[i].bt);
+          }
+        }
+        i = (i + 1) % temp.length;
+        k++;
+      }
+      return;
+    }
+
+    function checkDone(checkArr) {
+      for (let x = 0; x < checkArr.length; x++) {
+        if (checkArr[x].done === false) {
+          alldone = false;
+          return;
+        }
+      }
+      alldone = true;
+    }
+
+    function isBTzero(Btarr) {
+      for (let y = 0; y < Btarr.length; y++) {
+        if (Btarr[y].bt !== 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    function Roundrobin() {
+      let i = 0;
+      arrange();
+      while (!alldone) {
+        arrange();
+        checkDone(temp);
+        if (alldone === true) {
+          break;
+        }
+        if (isBTzero(arr)) {
+          time++;
+          i = 0;
+          continue;
+        } else {
+          if (arr[i].done == false) {
+            if (arr[i].bt <= quantum) {
+              time += arr[i].bt;
+              arr[i].bt = 0;
+              arr[i].ct = time;
+              arr[i].tat = arr[i].ct - arr[i].at;
+              arr[i].wt = arr[i].tat - BT[i];
+              arr[i].done = true;
+            } else {
+              arr[i].bt -= quantum;
+              time += quantum;
+            }
+          }
+        }
+        i = (i + 1) % arr.length;
+      }
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].bt = BT[i];
+      }
+    }
+
+    Roundrobin();
+
+    const newTable = new Result({
+      result: arr,
+    });
+
+    await newTable.save();
+    res.render("rrSolution", {
+      title: "RR Simulator",
+      arr: arr,
+      time: time,
+      flag: flag,
+    });
+  } else {
+    res.redirect("/");
+  }
+};
