23f833bb09bb76f2d1831c6ffc50ce01f2847ad0
Pras2214 <parikhprasann@gmail.com>
2023-03-14 16:46:40 +0530

    did some minor changes in css

--- server/controller/scanController.js
+++ server/controller/scanController.js
@@ -1,15 +1,17 @@
-exports.getScan = (req,res,next) => {
-    res.render("scan-cScan" , {
-        title : "Scan / cScan"
-    })
-}
+exports.getScan = (req, res, next) => {
+  res.render("scan-cScan", {
+    title: "Scan / cScan",
+  });
+};
 
-exports.postScan = (req,res,next) => {
-    let pId = req.body.pId
-    let tn = req.body.tn
-    let mode = req.body.mode
-    
-    console.log(pId , tn)
-    console.log(mode)
-    res.redirect("/")
-}
+exports.postScan = (req, res, next) => {
+  let pId = req.body.pId;
+  let tn = req.body.tn;
+  let mode = req.body.mode;
+  let direction = req.body.direction;
+
+  console.log(pId, tn);
+  console.log(mode);
+  console.log(direction);
+  res.redirect("/");
+};
