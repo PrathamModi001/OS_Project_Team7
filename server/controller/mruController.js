a0dd92a50b7417ec73f9285a14b8b1fddd42470a
Pras2214 <parikhprasann@gmail.com>
2023-03-29 10:31:03 +0530

    check the changelog file

--- server/controller/mruController.js
+++ server/controller/mruController.js
@@ -1,100 +1,105 @@
-const MRU = require("../models/mru")
+const MRU = require("../models/mru");
 
 /** GET MRU Page */
 exports.getMRU = (req, res, next) => {
-    res.render("MRU", {
-        title: "MRU"
-    })
-}
+  res.render("MRU", {
+    title: "MRU",
+  });
+};
 
 exports.postMRU = (req, res, next) => {
+  let frame = parseInt(req.body.frame);
+  // console.log(frame)
 
-    let frame = parseInt(req.body.frame);
-    // console.log(frame)
+  let totalPgs = [];
+  let totalPages = [];
+  let pages = new Array(frame);
+  for (let index = 0; index < frame; index++) {
+    pages[index] = "-";
+  }
 
-    let totalPgs = [];
-    let totalPages = [];
-    let pages = new Array(frame);
-    for (let index = 0; index < frame; index++) {
-        pages[index] = "-";
+  const reference = req.body.reference.split(" ");
+  const refString = reference.map(function (str) {
+    return parseInt(str);
+  });
+
+  let index = 0;
+  let recentIndex;
+  let didHit = [];
+  let hit = 0;
+  let pageFault = 0;
+
+  for (let i = 0; i < refString.length; i++) {
+    if (index === frame) {
+      if (doesExist(pages, refString[i])) {
+        didHit[i] = true;
+        hit++;
+        recentIndex = findIndex(pages, refString[i]);
+      } else {
+        didHit[i] = false;
+        pageFault++;
+        pages[recentIndex] = refString[i];
+      }
+    } else {
+      if (doesExist(pages, refString[i])) {
+        didHit[i] = true;
+        hit++;
+        recentIndex = findIndex(pages, refString[i]);
+      } else {
+        didHit[i] = false;
+        pageFault++;
+        pages[index] = refString[i];
+        recentIndex = index;
+        index++;
+      }
     }
+    totalPgs = totalPgs.concat(pages);
+    // console.log(pages)
+  }
+  totalPages = createNewArray(totalPgs, frame);
 
-    const reference = req.body.reference.split(' ');
-    const refString = reference.map(function (str) {
-        return parseInt(str);
-    });
-
-    let index = 0;
-    let recentIndex;
-    let hit = 0;
-    let pageFault = 0;
-
-    for (let i = 0; i < refString.length; i++) {
-        if (index === frame) {
-            if (doesExist(pages, refString[i])) {
-                hit++;
-                recentIndex = findIndex(pages, refString[i]);
-            } else {
-                pageFault++;
-                pages[recentIndex] = refString[i];
-            }
-        } else {
-            if (doesExist(pages, refString[i])) {
-                hit++;
-                recentIndex = findIndex(pages, refString[i]);
-            } else {
-                pageFault++;
-                pages[index] = refString[i];
-                recentIndex = index;
-                index++;
-            }
-        }
-        totalPgs = totalPgs.concat(pages)
-        // console.log(pages)
+  function doesExist(array, element) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === element) {
+        return true;
+      }
     }
-    totalPages = createNewArray(totalPgs, frame)
+    return false;
+  }
 
-    function doesExist(array, element) {
-        for (let j = 0; j < array.length; j++) {
-            if (array[j] === element) {
-                return true;
-            }
-        }
-        return false;
+  function createNewArray(totalPgs, frame) {
+    const totalPages = [];
+    for (let i = 0; i < totalPgs.length; i += frame) {
+      totalPages.push(totalPgs.slice(i, i + frame));
     }
+    return totalPages;
+  }
 
-    function createNewArray(totalPgs, frame) {
-        const totalPages = [];
-        for (let i = 0; i < totalPgs.length; i += frame) {
-            totalPages.push(totalPgs.slice(i, i + frame));
-        }
-        return totalPages;
+  function findIndex(array, element) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === element) {
+        return i;
+      }
     }
+    return null;
+  }
 
-    function findIndex(array, element) {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === element) {
-                return i;
-            }
-        }
-        return null;
-    }
+  const newMRU = new MRU({
+    refString: refString,
+    frame: frame,
+    hit: hit,
+    pageFault: pageFault,
+    totalPages: totalPages,
+  });
+  newMRU.save();
 
-    const newMRU = new MRU({
-        refString: refString,
-        frame: frame,
-        hit: hit,
-        pageFault: pageFault,
-        totalPages: totalPages,
-    })
-    newMRU.save();
-    
-    res.render("MRUsolution", {
-        title: "MRU-Solution",
-        refString: refString,
-        frame: frame,
-        totalPages: totalPages,
-        hit: hit,
-        pageFault: pageFault
-    })
-}
+  res.render("MRUsolution", {
+    title: "MRU-Solution",
+    refString: refString,
+    frame: frame,
+    totalPages: totalPages,
+    hit: hit,
+    pageFault: pageFault,
+    didHit: didHit,
+  });
+};
